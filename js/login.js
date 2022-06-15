d902695a2741e927b7e9ec643473f5ec9896d819
rita <maria.rita27012@gmail.com>
2022-06-15 15:30:42 -0300

    commit change all 2

--- js/login.js
+++ js/login.js
@@ -3,19 +3,19 @@ const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
 
 const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
+    if (target.value.length > 2) {
     button.removeAttribute('disabled');
     return;
-  }
+}
 
-  button.setAttribute('disabled', '');
+    button.setAttribute('disabled', '');
 }
 
 const handleSubmit = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html';
 }
 
 input.addEventListener('input', validateInput);
