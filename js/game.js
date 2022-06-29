e19061d166ca1252d7080a7202e6d17679762542
rita <maria.rita27012@gmail.com>
2022-06-29 12:05:50 -0300

    commit js card

--- js/game.js
+++ js/game.js
@@ -1,7 +1,25 @@
+const grid = document.querySelector('.grid');
+
+
+
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+
 const createCard = () => {
 
-    const card = documet.createElement('div');
-    const front = documet.createElement('div');
-    const back = documet.createElement('div');
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
 
 }
