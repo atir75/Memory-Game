e0d61d01f7df5528495bc7a8eb0d06248a43e285
rita <maria.rita27012@gmail.com>
2022-06-29 09:21:23 -0300

    commit no bug

--- js/login.js
+++ js/login.js
@@ -15,7 +15,7 @@ const handleSubmit = (event) => {
     event.preventDefault();
 
     localStorage.setItem('player', input.value);
-    window.location = 'pages/game.html';
+    window.location = 'game.html';
 }
 
 input.addEventListener('input', validateInput);
