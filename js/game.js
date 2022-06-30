866ed310c07f50e413c6c37cd9c000cd1efc9f8b
rita <maria.rita27012@gmail.com>
2022-06-30 09:15:01 -0300

    commit imagens

--- js/game.js
+++ js/game.js
@@ -27,6 +27,7 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
+    front.style.backgroundImage = `url('../imagens/${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
@@ -36,13 +37,10 @@ const createCard = (character) => {
 
 
 const loadGame = () => {
-    const duplicateCharacters = [ ...characters, ...characters ];
 
-    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
-
-    shuffledArray.forEach((character) => {
-    const card = createCard(character);
-    grid.appendChild(card);
+    characters.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
     });
 }
 
